f05d67a88aa9a42b9f5db2a2e4ebedd0b59adfcf
Papasotiropoulos Georgios <gpapasotiropoulos@gmail.com>
2025-12-26 17:54:44 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,34 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
+import { Link } from "gatsby";
 import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
-
-const baseUrl = "http://localhost:8000";
 const pages = [
-  { name: "Paper List", href: `${baseUrl}/` },
-  { name: "Further Material", href: `${baseUrl}/material` },
-//  { name: "How to Contribute", href: `${baseUrl}/contribute` },
-  { name: "About", href: `${baseUrl}/about` },
+  { name: "Paper List", href: "/" },
+  { name: "Further Material", href: "/material" },
+  { name: "About", href: "/about" },
 ];
 
-const Header = () => {
-  return (
-    <AppBar position="static" color="primary">
-      <Toolbar disableGutters>
-        <Typography
-          variant="h5"
-          noWrap
-          component="div"
-          sx={{ ml: 2, mr: 4, fontWeight: "bold" }}
+const Header = () => (
+  <AppBar position="static" color="primary">
+    <Toolbar disableGutters>
+      <Typography variant="h5" noWrap sx={{ ml: 2, mr: 4, fontWeight: "bold" }}>
+        Liquid Democracy in Social Choice
+      </Typography>
+      {pages.map((page) => (
+        <Button
+          key={page.name}
+          sx={{ mr: 1 }}
+          color="inherit"
+          component={Link}
+          to={page.href}  // relative href; Gatsby applies pathPrefix automatically
         >
-Liquid Democracy in Social Choice 
-        </Typography>
-        {pages.map((page) => (
-          <Button
-            key={page.name}
-            sx={{ mr: 1 }}
-            href={page.href}
-            color="inherit"
-            textAlign="center"
-          >
-            {page.name}
-          </Button>
-        ))}
-      </Toolbar>
-    </AppBar>
-  );
-};
+          {page.name}
+        </Button>
+      ))}
+    </Toolbar>
+  </AppBar>
+);
+
 export default Header;
